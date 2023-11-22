9f954542d7b0ed579d96af4622e67612465f51b0
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-11-21 17:00:16 -0700

    Creating the function to save to local storage

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,31 @@
 function Add() {
+
+    const getDataForm = (e) => {
+        e.preventDefault()
+
+        const target = e.target
+        const title = target.title.value
+        const description = target.description.value
+
+        const movie = {
+            id: new Date().getTime(),
+            title,
+            description
+        }
+        saveToStorage(movie)
+    }
+
+    const saveToStorage = (movie) => {
+        const movies = JSON.parse(localStorage.getItem('movies')) || []
+        movies.push(movie)
+        localStorage.setItem('movies', JSON.stringify(movies))
+    }
+
     return (
 
         <div className="add">
             <h3 className="title">Añadir pelicula</h3>
-            <form>
+            <form onSubmit={getDataForm}>
                 <input type="text" id="title" placeholder="Titulo" />
                 <textarea id="description" placeholder="Descripción"></textarea>
                 <input type="submit" id="save" value="Guardar" />
